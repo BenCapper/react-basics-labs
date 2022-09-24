3a0b8ae1f37324e139ccbaac4fce89a5802cb820
Ben Capper <Bencapper19@gmail.com>
2022-09-24 11:36:06 +0100

    Week 3 Exercises Done
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- tasky/src/components/Form.js
+++ tasky/src/components/Form.js
@@ -1,26 +1,83 @@
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+
+
 const AddTaskForm = (props) => {
 
     return (
+      <Box
+      component="form"
+      sx={{
+        '& .MuiOutlinedInput-root': { m: 1, width: '30ch' },
+      }}
+      onSubmit={props.submit}
+    >
       <div>
-        <form onSubmit={props.submit} onChange={(event) => props.change(event)}>
-          <label>
-              Task title:
-              <input type="text" name="title" required onChange={(event) => props.change(event)} />
-          </label>
-          <br />
-          <label>
-              Due date:
-              <input type="date" name="deadline" required onChange={(event) => props.change(event)} />
-          </label>
-          <br />
-          <label>
-              Details:
-              <input type="text" name="description" onChange={(event) => props.change(event)}/>
-          </label>
-          <input type="submit" value="Submit" />
-          
-          </form>
+        <TextField
+          required
+          id="outlined-required"
+          name="title"
+          label="Task Title"
+          InputLabelProps={{ shrink: true }}
+          onChange={(event) => props.change(event)}
+        />
       </div>
+      <div>
+        <TextField
+          required
+          name="deadline"
+          label="Deadline"
+          InputLabelProps={{ shrink: true }}
+          type="date"
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+      <FormControl fullWidth>
+        <InputLabel>Priority</InputLabel>
+        <Select
+          value={props.priority}
+          name="priority"
+          onChange={(event) => props.change(event)}
+        >
+          <MenuItem value={"Low"}>Low</MenuItem>
+          <MenuItem value={"Medium"}>Medium</MenuItem>
+          <MenuItem value={"High"}>High</MenuItem>
+        </Select>
+      </FormControl>
+      </div>
+      <div>
+        <TextField
+          name="description"
+          id="outlined-multiline-static"
+          label="Task Details"
+          InputLabelProps={{ shrink: true }}
+          multiline
+          rows={4}
+          onChange={(event) => props.change(event)}
+        />
+      </div>
+      <div>
+        <Button 
+          type="submit"
+          variant="contained"
+          color="primary"
+          sx={{
+            m: 1,
+            p: 1,
+            width: '95%'
+          }}
+        >
+          Add Task
+        </Button>
+      </div>
+    </Box>
     )
   };
   
